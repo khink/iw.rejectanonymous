da310ec225dfe6c0ab55a33449f8326781d141ab
Kees Hink <keeshink@gmail.com>
2018-06-20 15:17:12 +0200

    Proof of concept: Put site behind HTTP Basic Auth without logging in user.

--- iw/rejectanonymous/__init__.py
+++ iw/rejectanonymous/__init__.py
@@ -99,10 +99,57 @@ def rejectAnonymous(portal, request):
                 ):
             raise Unauthorized, "Anonymous rejected"
 
+
+def show_basicauth_popup(request):
+    """Show basic auth popup."""
+    realm = request.response.realm
+    request.response.setStatus(401)
+    request.response.setHeader(
+        'WWW-Authenticate',
+        'basic realm="%s"' % realm, 1)
+    request.response.setBody("""
+<html><head><title>Login</title></head><body><p>
+    This site has been password-walled.
+    Your site user credentials are invalid here.
+    Contact your site administrator for credentials.
+</p></body></html>""")
+
+
+def basicauth_validate(username_password_tuple):
+    """Check that basic auth-supplied username + password is valid."""
+    username, password = username_password_tuple
+    # username doesn't matter
+    if password == 'henk':
+        return True
+
+
+def reject_missing_password(portal, request):
+    """Check for passwordwall cookie / basicauth creds."""
+    cookie_name = '__passwordwall'
+    # Copied from rejectAnonymous
+    if request['REQUEST_METHOD'] == 'OPTIONS':
+        return
+    # Don't ask again if already logged in
+    if not isAnonymousUser():
+        return
+    if request.cookies.get(cookie_name):
+        return
+    name_password_tuple = request._authUserPW()
+    if not name_password_tuple:
+        show_basicauth_popup(request)
+        return
+    if not basicauth_validate(name_password_tuple):
+        show_basicauth_popup(request)
+    request.response.setCookie(cookie_name, 'content doesnt matter', path='/')
+
+
 def insertRejectAnonymousHook(portal, event):
     """ """
     try:
-        event.request.post_traverse(rejectAnonymous, (portal, event.request))
+        event.request.post_traverse(
+            reject_missing_password,
+            (portal, event.request),
+        )
     except RuntimeError:
         # Make this work in a testrunner
         pass
